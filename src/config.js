015fc40392d253a72ca29396312b411342c17eb3
guillermo-politecnico <58904456+guillermo-politecnico@users.noreply.github.com>
2020-05-14 01:12:13 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export const API = {
     trackingEndPoint: `https://us-central1-ecommercewidget-265813.cloudfunctions.net/bfc/track`,
     // trackingEndPoint: `http://localhost:5001/better-food-choices/us-central1/bfc/track`,
 
-    endpoint: gtin => `https://eatfit-service.foodcoa.ch/products/${gtin}/?resultType=array`,
+    endpoint: gtin => `https://scorethical-5fa1d.firebaseio.com/0${gtin}`,
 
 }
 
