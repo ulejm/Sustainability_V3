36b3a23cea0f95e81d6f319d1528cf5667a4556e
basilweiss <40361606+basilweiss@users.noreply.github.com>
2020-08-21 19:05:56 +0200

    Update taskDesc.js

--- src/BetterChoices/taskDesc.js
+++ src/BetterChoices/taskDesc.js
@@ -1,11 +1,11 @@
 export default [(group, country)=>`
 
 <h1>Aufgabenbeschreibung</h1>
-<p>Liebe Teilnehmerin, <br/>lieber Teilnehmer,</p>
+<p>Liebe Teilnehmerin, lieber Teilnehmer</p>
 
-<p>heute kaufen Sie Lebensmittel in einem Online-Supermarkt ein. </p><p>
+<p>Heute kaufen Sie Lebensmittel in einem Online-Supermarkt ein. </p>
 
-Wir bitten Sie, ihren <b>wöchentlichen Lebensmitteleinkauf</b> in dem Online-Supermarkt durchzuführen, also Lebensmittel einzukaufen, die Sie <b>für sich selbst für den Zeitraum von einer Woche einkaufen</b> würden. Dazu steht Ihnen ein <b>Budget von insgesamt ${country == 'de' ? '€55':'CHF 100,-'}</b> zur Verfügung. Gehen Sie bitte davon aus, dass Sie einen <b>gewöhnlichen Einkauf durchführen</b>. Außergewöhnliche Ereignisse, die in der kommenden Woche anstehen könnten (z.B. ein geplantes gemeinsames Essen mit Freunden), bitten wir Sie, nicht in ihren Einkauf einzubeziehen.
+<p>	Wir bitten Sie die Lebensmittel einzukaufen, die Sie <b>für sich selbst für den Zeitraum von zwei Tagen</b> einkaufen würden. Dazu steht Ihnen ein <b>Budget von maximal ${country == 'de' ? '€15':'CHF 25.-'}</b> zur Verfügung. Gehen Sie bitte davon aus, dass Sie einen <b>gewöhnlichen Einkauf</b> durchführen. Aussergewöhnliche Ereignisse, die in der kommenden Woche anstehen könnten (z.B. ein geplantes gemeinsames Essen mit Freunden), bitten wir Sie, nicht in Ihren Einkauf einzubeziehen.
 </p>`,(group, country)=>`
 
 <p>Wenn Sie ein Produkt kaufen möchten, geht das <b>via Klick auf „Auf die Einkaufsliste“</b> oder <b>durch einen Klick auf das kleine „Plus“ in der Produktübersicht</b> (siehe unten). In die Detailansicht wechseln Sie, indem Sie auf das Bild des Produktes oder seinen Namen klicken. </p>
@@ -13,17 +13,15 @@ Wir bitten Sie, ihren <b>wöchentlichen Lebensmitteleinkauf</b> in dem Online-Su
 <img src="${chrome.runtime.getURL('task.png')}" width="300px"/>
 
 <p>Die Detailansicht liefert folgende Informationen zu dem Produkt:</p>
-<li style="padding-left:20px">Menge des Produktes</li>
-<li style="padding-left:20px">Detaillierte Abbildung</li>
+<li style="padding-left:20px">Name des Produktes</li>
+<li style="padding-left:20px">Detaillierte Abbildung und Produktbeschreibung</li>
 <li style="padding-left:20px">Preis</li>
-<li style="padding-left:20px">Nährwerte (100g und eine Portion)</li>
 <li style="padding-left:20px">Allgemeine Produktinformationen (Kühlung, Verpackungsart, Herkunft etc.) </li>
 <li style="padding-left:20px">Zutaten des Produktes</li>
-${group == 'A' || group == 'B' ? '<li style="padding-left:20px">Nutri-Score (fünfstufige Farb- und Buchstabenskala, die einen Überblick über die Nährwertqualität eines Produktes liefert (A-E); bezieht Energiegehalt, Zucker, gesättigte Fettsäuren, Natrium, Proteine, Ballaststoffe und Obst-, Gemüse- bzw. Nussanteil mit ein)<img style="max-height:800px" src="'+chrome.runtime.getURL('task-AB-ch.png')+'"/></li>':'<img style="max-height:800px" src="'+chrome.runtime.getURL('task-C-ch.png')+'"/>'}
+${group == 'A' ? '<li style="padding-left:20px">Nachhaltigkeitsbewertung dargestellt als fünfstufige Farb- und Zahlenskala, die einen Überblick über die Nachhaltigkeit eines Produktes liefert (-2, -1, 0, +1, +2). Die Nachhaltigkeitsbewertung bezieht Erderwärmung, Feinstaubbelastung, Wassernutzung, Bodennutzung, Tierwohl und soziale Aspekte mit ein. <img style="max-height:800px" src="'+chrome.runtime.getURL('task-AB-ch.png')+'"/></li>':'<img style="max-height:800px" src="'+chrome.runtime.getURL('task-C-ch.png')+'"/>'}
 
 
-<p>Die Artikel, die sich aktuell in ihrem Warenkorb befinden, können Sie jederzeit durch einen Klick auf das Einkaufswagen-Symbol am oberen rechten Rand einsehen. Dort können Sie auch bereits gewählte Produkte wieder entfernen.
-Über das „i“-Symbol können Sie diese Aufgabenbeschreibung jederzeit erneut aufrufen. </p>
+<p>Die Artikel, die sich aktuell in ihrem Warenkorb befinden, können Sie jederzeit durch einen Klick auf das Einkaufswagen-Symbol am oberen rechten Bildrand einsehen. Dort können Sie auch bereits gewählte Produkte wieder entfernen. Über das „i“-Symbol können Sie diese Aufgabenbeschreibung jederzeit erneut aufrufen. </p>
 
-<p>Sie haben sehr viel Zeit für die Aufgabe, sodass Sie sich in Ruhe alle Produkte und Kategorien ansehen können. Wenn Sie ein Produkt kaufen möchten, fügen Sie es Ihrem Warenkorb hinzu. Sobald Sie alle gewünschten Produkte hinzugefügt haben, klicken Sie auf das Einkaufswagen-Symbol am oberen rechten Rand und wählen unten „Zur Kasse“.<img src="${chrome.runtime.getURL("task-2.png")}" width="100px"/></p>
-`, (group, country) => country == 'ch' ? '<p><b>Als zusätzliche Vergütung für die Teilnahme an der Studie werden unter allen Teilnehmenden drei Personen ausgelost, die ihren zusammengestellten Warenkorb zugeschickt bekommen.</b> Geben Sie dazu bitte am Ende der Umfrage Ihre E-Mail-Adresse an, damit wir Sie im Gewinnfall kontaktieren können. Die E-Mail-Adresse dient nur zur Benachrichtigung, wird getrennt von Ihren Antworten gespeichert und nach der Studie gelöscht. </p>' : '<p><b>Als zusätzliche Vergütung für die Teilnahme an der Studie werden unter allen Teilnehmenden drei Personen ausgelost, die ihren zusammengestellten Warenkorb zugeschickt bekommen.</b> Sollte ein Produkt nicht verfügbar sein, wird es durch ein Alternativprodukt ersetzt, das dem ursprünglichen mindestens gleichwertig ist. Um Ihren Gewinn erhalten zu können, geben Sie bitte am Ende der Umfrage Ihre E-Mail-Adresse an, damit wir Sie im Falle eines Gewinns kontaktieren können. Die E-Mail-Adresse dient nur zur Benachrichtigung, wird getrennt von Ihren Antworten gespeichert und nach der Studie gelöscht.</p>']
+<p>Wenn Sie ein Produkt kaufen möchten, fügen Sie es zu Ihrem Warenkorb hinzu. Sobald Sie alle gewünschten Produkte hinzugefügt haben, klicken Sie auf das Einkaufswagen-Symbol am oberen rechten Rand und wählen unten „Zur Kasse“.<img src="${chrome.runtime.getURL("task-2.png")}" width="100px"/></p>
+`, (group, country) => country == 'ch' ? '<p><b>Als Vergütung für die Teilnahme an der Studie werden unter allen Teilnehmenden drei Personen ausgelost, die ihren zusammengestellten Warenkorb erhalten.</b> Geben Sie dazu bitte am Ende der Umfrage Ihre E-Mail-Adresse an, damit wir Sie im Gewinnfall kontaktieren können. Die E-Mail-Adresse dient nur zur Benachrichtigung und wird nach der Studie gelöscht. </p>' : '<p><b>Als zusätzliche Vergütung für die Teilnahme an der Studie werden unter allen Teilnehmenden drei Personen ausgelost, die ihren zusammengestellten Warenkorb erhalten.</b> Geben Sie dazu bitte am Ende der Umfrage Ihre E-Mail-Adresse an, damit wir Sie im Falle eines Gewinns kontaktieren können. Die E-Mail-Adresse dient nur zur Benachrichtigung und wird nach der Studie gelöscht.</p>']
